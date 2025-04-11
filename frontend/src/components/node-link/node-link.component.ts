209b6815e81c2ac13c2aca8b05db3bd8077c348b
velitchko <velitchkofilipov@gmail.com>
2025-04-11 10:05:27 +0200

    update node-link component to conditionally render edges based on variant

--- frontend/src/components/node-link/node-link.component.ts
+++ frontend/src/components/node-link/node-link.component.ts
@@ -23,7 +23,7 @@ export class NodeLinkComponent implements AfterViewInit {
     };
     private width = 1400 - this.margin.left - this.margin.right;
     private height = 1200 - this.margin.top - this.margin.bottom;
-
+   
     private nodes: d3.Selection<SVGCircleElement, NodeExt, SVGGElement, any>;
     private edges: d3.Selection<SVGLineElement, EdgeExt, SVGGElement, any>;
     private buffer: d3.Selection<SVGCircleElement, NodeExt, SVGGElement, any>;
@@ -31,21 +31,23 @@ export class NodeLinkComponent implements AfterViewInit {
     private simulation: d3.Simulation<NodeExt, EdgeExt>;
     private aesthetics: Aesth;
     private graph: { nodes: NodeExt[], edges: EdgeExt[] };
+    private config = {
+        dataset: '',
+        variant: '',
+        level: '',
+        task: '',
+    };
 
     constructor(private dataService: DataService, private errorHandler: GlobalErrorHandler, private resultsService: ResultsService) { 
     }
 
     async ngAfterViewInit(): Promise<void> {
-        // const meta = d3.select('#metadata').text().trim();
-        // const dataset = meta.split('-')[0];
-        // const variant = meta.split('-')[1];
-        // const level = meta.split('-')[2];
-        // const task = meta.split('-')[3];
+        const meta = d3.select('#metadata').text().trim();
+        this.config.dataset = meta.split('-')[0]; // dataset name
+        this.config.variant = meta.split('-')[1]; // link, nolink, or interaction
+        this.config.level = meta.split('-')[2]; // complexity level (low, high)
+        this.config.task = meta.split('-')[3]; // task code t1, t2, t3, t4, t5, t6
 
-        // const finalLevel = this.resultsService.getFinalLevel(task, level);
-        // const fileName = `${dataset}_${variant}.${finalLevel}.json`;
-
-        // const graph = await this.dataService.loadFilename(fileName);
 
         // this.aesthetics = graph.aesthetics;
         this.graph = lesMis as { nodes: NodeExt[], edges: EdgeExt[] };
@@ -116,7 +118,38 @@ export class NodeLinkComponent implements AfterViewInit {
                 .attr('class', 'node')
                 .attr('r', (d: NodeExt) => 10)
                 .attr('fill', (d: NodeExt) => 'black')
-                .style('opacity', 0);
+                .style('opacity', 0)
+                .on('mouseover', (event: MouseEvent, d: NodeExt) => {
+                    if(this.config.variant !== 'interaction') return;
+                    d3.select(event.currentTarget as SVGCircleElement).attr('r', 15).attr('fill', 'red');
+                    d3.selectAll<SVGLineElement, EdgeExt>('.edges line')
+                        .filter((edge: EdgeExt) => edge.source.id === d.id || edge.target.id === d.id)
+                        .attr('stroke', 'red')
+                        .attr('stroke-width', 4)
+                        .style('opacity', 1);
+
+                    d3.selectAll<SVGCircleElement, NodeExt>('.nodes circle')
+                        .filter((node: NodeExt) => graph.edges.some(edge => (edge.source.id === d.id && edge.target.id === node.id) || (edge.target.id === d.id && edge.source.id === node.id)))
+                        .attr('fill', 'red');
+                    d3.selectAll<SVGTextElement, NodeExt>('.labels text.label').filter((label: NodeExt) => label.id === d.id).attr('stroke', 'red').attr('fill', 'red');
+                }
+                )
+                .on('mouseout', (event: MouseEvent, d: NodeExt) => {
+                    if(this.config.variant !== 'interaction') return;
+                    d3.select(event.currentTarget as SVGCircleElement).attr('r', 10).attr('fill', 'black');
+                    d3.selectAll<SVGLineElement, EdgeExt>('.edges line')
+                        .attr('stroke', 'black')
+                        .attr('stroke-width', 2)
+                        .style('opacity', 0);
+
+                    d3.selectAll<SVGCircleElement, NodeExt>('.nodes circle')
+                        .attr('fill', 'black');
+
+                    d3.selectAll<SVGTextElement, NodeExt>('.labels text.label')
+                        .attr('stroke', 'black')
+                        .attr('fill', 'black');
+                }
+                );
 
         // initialize labels
         this.labels = svg.append('g')
@@ -131,12 +164,13 @@ export class NodeLinkComponent implements AfterViewInit {
                 .attr('fill', 'black')
                 .attr('dy', '0.35em')
                 .attr('text-anchor', 'middle')
+                .attr('pointer-events', 'none')
                 .style('font-family', '\'Fira Mono\', monospace')
                 .style('opacity', 0);
 
         this.simulation = d3.forceSimulation(graph.nodes)
             .force('link', d3.forceLink(graph.edges).id((d: any) => (d as NodeExt).id).links(graph.edges))
-            .force('charge', d3.forceManyBody().strength(-300))
+            .force('charge', d3.forceManyBody().strength(-600))
             .force('center', d3.forceCenter(this.width / 2, this.height / 2))
             .on('tick', () => {
                 this.edges
@@ -155,10 +189,12 @@ export class NodeLinkComponent implements AfterViewInit {
 
                 this.labels
                     .attr('x', (d: NodeExt) => d.x)
-                    .attr('y', (d: NodeExt) => d.y);
+                    .attr('y', (d: NodeExt) => d.y + 16);
             })
             .on('end', () => {
-                this.edges.style('opacity', 1);
+                if(this.config.variant === 'nodelink') {
+                    this.edges.style('opacity', 1);
+                }
                 this.nodes.style('opacity', 1);
                 this.buffer.style('opacity', 1);
                 this.labels.style('opacity', 1);
