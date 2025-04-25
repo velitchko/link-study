60066891c7bf14ed3fa83b95340effe18c7ddcf9
velitchko <velitchkofilipov@gmail.com>
2025-04-25 10:16:49 +0200

    save answers to results when progressing through survey

--- frontend/src/components/survey/survey.component.ts
+++ frontend/src/components/survey/survey.component.ts
@@ -66,7 +66,41 @@ export class SurveyComponent {
 
         // update end time and record result
         this.timer.end = Date.now();
-        const time = this.timer.end - this.timer.start;
+
+        this.survey.onCurrentPageChanged.add((sender, options) => {
+            console.log(options.oldCurrentPage.name)
+            if (options.oldCurrentPage.name.includes('feedback') && options.oldCurrentPage.name !== 'qualitative-feedback') {
+                // push to results
+                this.resultsService.pushResult({
+                    index: -99,
+                    time: 0,
+                    task: options.oldCurrentPage.name.split('-')[2],
+                    encoding: this.resultsService.getUserParams()?.encoding || 'unknown',
+                    complexity: this.resultsService.getUserParams()?.complexity || 'unknown',
+                    answer: sender.data[`${options.oldCurrentPage.name}`]
+                }, true);
+
+                // reset start time
+                this.timer.start = Date.now();
+                return;
+            }
+
+            // update end time and record result
+            this.timer.end = Date.now();
+            const time = this.timer.end - this.timer.start;
+
+
+            // push to results
+            this.resultsService.pushResult({
+                index: options.oldCurrentPage.visibleIndex,
+                time: time,
+                task: options.oldCurrentPage.name.split('-')[2],
+                // GET SUBSTRING FROM START TO options.newCurrentPage.name.split('-')[options.newCurrentPage.name.split('-').length - 1]
+                encoding: options.oldCurrentPage.name.split('-')[0],
+                complexity: this.resultsService.getUserParams()?.complexity || 'unknown',
+                answer: sender.data[`${options.oldCurrentPage.name}-answer`] || this.resultsService.getAnswers(options.oldCurrentPage.name.split('-')[2])
+            });
+        });
 
 
 
